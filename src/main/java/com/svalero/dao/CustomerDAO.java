6bce99e0d3ea07cb514a7dd9e3003819f0699123
Fandikoos <danfandos@gmail.com>
2024-01-30 12:26:05 +0100

    Agragando cambios en la rama feature

--- src/main/java/com/svalero/dao/CustomerDAO.java
+++ src/main/java/com/svalero/dao/CustomerDAO.java
@@ -10,6 +10,7 @@ import java.util.List;
 
 public interface CustomerDAO {
 
+    //Probando git y github, mergeando etc
     @SqlQuery("SELECT * FROM clientes")
     @UseRowMapper(CustomerMbapper.class)
     List<Customer> getCustomers();
